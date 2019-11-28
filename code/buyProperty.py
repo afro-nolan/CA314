8cb20f1b96e1fbf24513897467dc7d08715fe70d
Dopamu <55927934+Dopamu@users.noreply.github.com>
2019-11-28 13:03:24 +0000

    Update buyProperty.py

--- code/buyProperty.py
+++ code/buyProperty.py
@@ -1,40 +1,39 @@
 import pyglet
 from pyglet.window import key
+import sys
 
-window = pyglet.window.Window()
-image  = pyglet.resource.image('resources/titledeed.jpg')
-
-label = pyglet.text.Label("                                         Enter B or A" ,
-                            font_name='Times New Roman',
-                            font_size = 18,
-                            x= window.width//2, y = window.height//2,
-                            anchor_x = 'center', anchor_y ='center')
+image  = pyglet.resource.image('titledeed.jpg')
 
 
+class Press(pyglet.window.Window):
 
-@window.event
-def on_key_press(symbol, modifiers):
-    if symbol == key.A:
-        print("A Key was pressed")
+    def __init__(self,*args,**kwargs):
+        pyglet.window.Window.__init__(self, *args,**kwargs)
 
-    elif symbol == key.B:
-        print("B key was pressed")
+        self.label = pyglet.text.Label("                                      Enter B or A" ,
+                                font_name='Times New Roman',
+                                font_size = 18,
+                                x= self.width//2, y = self.height//2,
+                                anchor_x = 'center', anchor_y ='center')
 
-    elif symbol == key.ENTER:
-        print("ENTER key was pressed")
+    def on_key_press(self, symbol, modifiers):
+        if symbol == key.A:
+            print("A Key was pressed")
 
+        elif symbol == key.B:
+            print("B key was pressed")
 
+        elif symbol == key.ENTER:
+            print("ENTER key was pressed") 
 
-@window.event
-def on_draw():
-    window.clear()
-    label.draw()
-    image.blit(0,0)
+    def on_draw(self):
+        print("The window was drawn")
+        sys.stdout.flush()
+        self.label.draw()
+        image.blit(0,0)
 
-
+press = Press()
 pyglet.app.run()
-
-
 
 
 
